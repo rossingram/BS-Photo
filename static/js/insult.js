a8e18680f97d11c1868d505d47b37660e01181eb
Ross Ingram <rosingram@gmail.com>
2012-07-14 23:32:51 -0600

    init

---
+++ static/js/insult.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+function text() {
+};
+
+text = new text();
+number = 0;
+
+// textArray
+text[number++] = "Just because you have a camera doesn't mean you need to take pictures professionally."
+text[number++] = "I hear KFC is hiring."
+text[number++] = "My Grandma takes better pictures than you."
+text[number++] = "LOOK AT MY NEW NIKON DSLR!!!!"
+text[number++] = "It's just a hobby."
+text[number++] = "Someone paid you to take those??"
+text[number++] = "The quickest way to make money at photography is to sell your camera."
+text[number++] = "Having and camera makes you no more a photographer than having a hammer and some nails makes you a carpenter."
+text[number++] = "How do you get the professional photographer off your front porch? Pay them for the pizza. "
+text[number++] = "Keep your day job."
+text[number++] = "10 MP don't mean shit."
+text[number++] = "Do you actually make money?"
+text[number++] = "BULLSHIT"
+text[number++] = "If you're a professional photographer then I'm a professional pope."
+text[number++] = "No, you cannot come to my wedding."
+
+
+
+
+
+
+
+// keep adding items here...
+
+increment = Math.floor(Math.random() * number);
